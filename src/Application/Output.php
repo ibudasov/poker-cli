1d6d0e876b0657c2b565731a5e75081492ffc2bf
igorbudasov <igor.budasov@mollie.com>
2020-11-24 20:37:03 +0100

    missing method in the interface

--- src/Application/Output.php
+++ src/Application/Output.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PokerCli\Application;
 
+use Poker\Card;
 use Poker\Hand;
 
 interface Output
@@ -11,4 +12,6 @@ interface Output
     public function println(string $line): void;
 
     public function outputMultipleHands(Hand ...$hands): void;
+
+    function cardToString(Card $card): string;
 }
